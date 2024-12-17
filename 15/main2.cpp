a54788cdfaae5e74a5853e12474d970010f5afd3
Piotr Głębocki <piotr.glebocki@paramount.com>
2024-12-17 23:28:12 +0100

    15 - solution 2 improved

--- 15/main2.cpp
+++ 15/main2.cpp
@@ -24,6 +24,10 @@ enum class Direction {
 struct Position {
     explicit Position(int row, int col) : row(row), col(col) {}
     int row, col;
+
+    bool operator!=(const Position &other) const {
+        return row != other.row || col != other.col;
+    }
 };
 
 vector<vector<char>> read_field(const string &filename) {
@@ -64,11 +68,20 @@ vector<Direction> read_moves(const string &filename) {
         for (auto c: line) {
             Direction dir;
             switch (c) {
-                case '<': dir = Direction::LEFT; break;
-                case '>': dir = Direction::RIGHT; break;
-                case '^': dir = Direction::UP; break;
-                case 'v': dir = Direction::DOWN; break;
-                default: continue; // Ignore invalid characters
+                case '<':
+                    dir = Direction::LEFT;
+                    break;
+                case '>':
+                    dir = Direction::RIGHT;
+                    break;
+                case '^':
+                    dir = Direction::UP;
+                    break;
+                case 'v':
+                    dir = Direction::DOWN;
+                    break;
+                default:
+                    continue; // Ignore invalid characters
             }
             output.push_back(dir);
         }
@@ -79,10 +92,14 @@ vector<Direction> read_moves(const string &filename) {
 
 std::tuple<int, int> get_step(Direction dir) {
     switch (dir) {
-        case Direction::LEFT:  return make_tuple(0, -1);
-        case Direction::RIGHT: return make_tuple(0, 1);
-        case Direction::UP:    return make_tuple(-1, 0);
-        case Direction::DOWN:  return make_tuple(1, 0);
+        case Direction::LEFT:
+            return make_tuple(0, -1);
+        case Direction::RIGHT:
+            return make_tuple(0, 1);
+        case Direction::UP:
+            return make_tuple(-1, 0);
+        case Direction::DOWN:
+            return make_tuple(1, 0);
     }
     return make_tuple(0, 0); // Default case
 }
@@ -107,33 +124,35 @@ Position find_robot(const vector<vector<char>> &matrix) {
     assert(false);
 }
 
-Position move_horozontally(vector<vector<char>> &matrix, const Position &robot, Direction dir) {
-    const auto &[row_step, col_step] = get_step(dir);
-    const auto new_row = robot.row + row_step;
-    const auto new_col = robot.col + col_step;
-    int r = new_row, c = new_col;
-
-    while (matrix[r][c] != WALL) {
-        if (matrix[r][c] == EMPTY) {
-            // Move the robot and clear the previous position
-            matrix[robot.row][robot.col] = EMPTY;
-            matrix[r][c] = ROBOT;
-            return Position(r, c);
-        }
-        r += row_step;
-        c += col_step;
-    }
-    return robot;
-}
-
-bool can_move_vertically(const vector<vector<char>> &matrix, const Position &pos, Direction dir) {
+bool move_horozontally(vector<vector<char>> &matrix, const Position &pos, Direction dir) {
     const auto &[row_step, col_step] = get_step(dir);
     const auto new_row = pos.row + row_step;
     const auto new_col = pos.col + col_step;
 
-    if (new_row < 0 || new_row >= matrix.size() || new_col < 0 || new_col >= matrix[0].size()) {
+    if (matrix[new_row][new_col] == WALL) {
         return false;
+    } else if (matrix[new_row][new_col] == EMPTY) {
+        matrix[new_row][new_col] = matrix[pos.row][pos.col];
+        matrix[pos.row][pos.col] = EMPTY;
+        return true;
+    } else {
+        if (move_horozontally(matrix, Position{new_row, new_col}, dir)) {
+            matrix[new_row][new_col] = matrix[pos.row][pos.col];
+            matrix[pos.row][pos.col] = EMPTY;
+            return true;
+        }
     }
+
+    return false;
+}
+
+bool can_move_vertically(const vector<vector<char>> &matrix, const Position &pos, Direction dir, vector<tuple<Position, char>>& to_be_moved) {
+    const auto &[row_step, col_step] = get_step(dir);
+    const auto new_row = pos.row + row_step;
+    const auto new_col = pos.col + col_step;
+
+    if (matrix[pos.row][pos.col] != WALL)
+        to_be_moved.push_back(make_tuple(pos, matrix[pos.row][pos.col]));
 
     char val = matrix[new_row][new_col];
     if (val == WALL) {
@@ -141,42 +160,26 @@ bool can_move_vertically(const vector<vector<char>> &matrix, const Position &pos
     } else if (val == EMPTY) {
         return true;
     } else if (val == BOX_L) {
-        return can_move_vertically(matrix, Position(new_row, new_col), dir) &&
-               can_move_vertically(matrix, Position(new_row, new_col + 1), dir);
+        return can_move_vertically(matrix, Position(new_row, new_col), dir, to_be_moved) &&
+               can_move_vertically(matrix, Position(new_row, new_col + 1), dir, to_be_moved);
     } else if (val == BOX_R) {
-        return can_move_vertically(matrix, Position(new_row, new_col), dir) &&
-               can_move_vertically(matrix, Position(new_row, new_col - 1), dir);
+        return can_move_vertically(matrix, Position(new_row, new_col), dir, to_be_moved) &&
+               can_move_vertically(matrix, Position(new_row, new_col - 1), dir, to_be_moved);
     }
     return false;
 }
 
-Position move_vertically(vector<vector<char>> &matrix, const Position &pos, Direction dir) {
-    const auto &[row_step, col_step] = get_step(dir);
-    const auto new_row = pos.row + row_step;
-    const auto new_col = pos.col + col_step;
-
-    if (matrix[new_row][new_col] == EMPTY) {
+bool move_vertically(vector<vector<char>> &matrix, vector<tuple<Position, char>> &to_be_moved, Direction dir) {
+    for (const auto& [pos, _] : to_be_moved) {
         matrix[pos.row][pos.col] = EMPTY;
-        matrix[new_row][new_col] = ROBOT;
-        return Position(new_row, new_col);
-    } else if (matrix[new_row][new_col] == BOX_L) {
-        const auto box_new_row = new_row + row_step;
-        const auto box_new_col_left = new_col;
-        const auto box_new_col_right = new_col + 1;
-
-        if (matrix[box_new_row][box_new_col_left] == EMPTY &&
-            matrix[box_new_row][box_new_col_right] == EMPTY) {
-            matrix[box_new_row][box_new_col_left] = BOX_L;
-            matrix[box_new_row][box_new_col_right] = BOX_R;
-
-            matrix[new_row][new_col] = ROBOT;
-            matrix[pos.row][pos.col] = EMPTY;
-
-            matrix[new_row][new_col + 1] = EMPTY;
-            return Position(new_row, new_col);
-        }
     }
-    return pos;
+
+     const auto &[row_step, col_step] = get_step(dir);
+    for (const auto& [pos, c] : to_be_moved) {
+         const auto new_row = pos.row + row_step;
+         const auto new_col = pos.col + col_step;
+         matrix[new_row][new_col] = c;
+     }
 }
 
 Position move(vector<vector<char>> &matrix, const Position &robot, Direction dir) {
@@ -186,13 +189,17 @@ Position move(vector<vector<char>> &matrix, const Position &robot, Direction dir
 
     if (matrix[new_row][new_col] == WALL) {
         return robot; // No movement
-    } else if (matrix[new_row][new_col] == EMPTY) {
-        return move_vertically(matrix, robot, dir);
-    } else if (matrix[new_row][new_col] == BOX_L) {
+    } else {
         if (dir == Direction::LEFT || dir == Direction::RIGHT) {
-            return move_horozontally(matrix, robot, dir);
-        } else if (can_move_vertically(matrix, Position(new_row, new_col), dir)) {
-            return move_vertically(matrix, robot, dir);
+            if (move_horozontally(matrix, robot, dir)) {
+                return Position(new_row, new_col);
+            }
+        } else {
+            vector<tuple<Position, char>> to_be_moved;
+            if (can_move_vertically(matrix, robot, dir, to_be_moved)) {
+                move_vertically(matrix, to_be_moved, dir);
+                return Position(new_row, new_col);
+            }
         }
     }
     return robot;
