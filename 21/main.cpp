cc562758c7889d154b108d319207054697154eaa
Piotr Głębocki <piotr.glebocki@paramount.com>
2024-12-22 11:15:14 +0100

    21 - solution

--- 21/main.cpp
+++ 21/main.cpp
@@ -1,12 +1,12 @@
-#include <unordered_map>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
-char NUMERIC_CONTROLLER[4][3]{
+vector<vector<char>> NUMERIC_CONTROLLER = {
     {
         '7', '8', '9'},
     {
@@ -14,146 +14,227 @@ char NUMERIC_CONTROLLER[4][3]{
     {
         '1', '2', '3'},
     {
-        ' ', '0', 'A'}
+        '#', '0', 'A'}
 };
 
-char KEYPAD_CONTROLLER[2][3]{
-    {' ', '^', 'A'},
+vector<vector<char>> KEYPAD_CONTROLLER = {
+    {'#', '^', 'A'},
     {'<', 'v', '>'},
+};
+
+const vector<tuple<int, int>> DIRS = {
+    {0,  1},
+    {0,  -1},
+    {1,  0},
+    {-1, 0}
 };
 
 struct Position {
     int r, c;
 
-    tuple<int, int> distance(const Position &other) const {
-        return make_tuple(
-            other.r - r,
-            other.c - c
-        );
+    char get_move_char_to(const Position &to) const {
+        if (r == to.r) {
+            return (to.c - c < 0) ? '<' : '>';
+        } else {
+            return (to.r - r < 0) ? '^' : 'v';
+        }
+    }
+
+    bool operator==(const Position &other) const {
+        return r == other.r && c == other.c;
     }
 };
 
-unordered_map<char, Position> numeric_map;
-unordered_map<char, Position> kaypad_map;
+Position find_char(const vector<vector<char>> &matrix, char target) {
+    for (int r = 0; r < matrix.size(); r++) {
+        for (int c = 0; c < matrix[0].size(); c++) {
+            if (matrix[r][c] == target) {
+                return Position{r, c};
+            }
+        }
+    }
+    assert(false); // not found
+}
 
-void init_numeric_controller_map() {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 3; j++) {
-            numeric_map.insert({NUMERIC_CONTROLLER[i][j], Position{i, j}});
+vector<Position> get_neighbours(const vector<vector<char>> &matrix, const Position &pos) {
+    const auto rows = matrix.size();
+    const auto cols = matrix[0].size();
+    const auto &[pos_r, pos_c] = pos;
+
+    vector<Position> neighbours;
+    for (const auto &[dr, dc]: DIRS) {
+        int r = pos_r + dr;
+        int c = pos_c + dc;
+
+        if (0 <= r && r < rows && 0 <= c && c < cols && matrix[r][c] != '#') {
+            neighbours.push_back(Position{r, c});
+        }
+    }
+    return neighbours;
+}
+
+void dfs(
+    const vector<vector<char>> &matrix,
+    int depth,
+    const Position &pos,
+    const Position &target,
+    vector<Position> current_path,
+    vector<vector<Position>> &paths
+) {
+    if (depth > 5) return;
+
+    if (pos == target) {
+        paths.push_back(current_path);
+        return;
+    }
+
+    auto neighbours = get_neighbours(matrix, pos);
+    for (const auto &neighbour: neighbours) {
+        if (std::find(current_path.begin(), current_path.end(), neighbour) == current_path.end()) {
+            auto new_path = current_path;
+            new_path.push_back(neighbour);
+            dfs(matrix, depth + 1, neighbour, target, new_path, paths);
         }
     }
 }
 
-void init_keypad_controller_map() {
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            kaypad_map.insert({KEYPAD_CONTROLLER[i][j], Position{i, j}});
+bool compare_paths_len(const vector<char> &a, const vector<char> &b) {
+    return a.size() < b.size();
+}
+
+vector<vector<char>> find_shortest_paths(vector<vector<char>> &paths) {
+    vector<vector<char>> out;
+    sort(paths.begin(), paths.end(), compare_paths_len);
+
+    if (!paths.empty()) {
+        size_t max_size = paths.front().size();
+        for (const auto &path: paths) {
+            if (max_size == path.size()) {
+                out.push_back(path);
+            } else {
+                break;
+            }
+        }
+    }
+    return out;
+}
+
+vector<vector<char>> find_simplest_paths(vector<vector<char>> &paths) {
+    assert(!paths.empty());
+    vector<vector<char>> out;
+    int min_number_of_changes = INT_MAX;
+
+    for (int p_index = 0; p_index < paths.size(); p_index++) {
+        const auto &path = paths[p_index];
+        int number_of_changes = 0;
+
+        for (int i = 1; i < path.size(); i++) {
+            if (path[i] != path[i - 1]) {
+                number_of_changes++;
+            }
+        }
+
+        if (number_of_changes < min_number_of_changes) {
+            out.clear();
+            out.push_back(path);
+            min_number_of_changes = number_of_changes;
+        } else if (number_of_changes == min_number_of_changes) {
+            out.push_back(path);
+        }
+    }
+
+    return out;
+}
+
+vector<vector<char>> find_all_move_sequences(const vector<vector<char>> &matrix, char from, char to) {
+    const auto rows = matrix.size();
+    const auto cols = matrix[0].size();
+
+    auto start = find_char(matrix, from);
+    auto end = find_char(matrix, to);
+
+    vector<vector<Position>> paths;
+    vector<vector<bool>> visited(rows, vector(cols, false));
+    dfs(matrix, 0, start, end, {start}, paths);
+
+    vector<vector<char>> sequences;
+
+    for (const auto &path: paths) {
+        vector<char> sequence;
+        auto prev = path.front();
+        for (int i = 1; i < path.size(); i++) {
+            auto current = path[i];
+            auto sign = prev.get_move_char_to(current);
+            sequence.push_back(sign);
+            prev = current;
+        }
+        sequences.push_back(sequence);
+    }
+
+    return sequences;
+}
+
+vector<string> get_path_sequences(const vector<vector<char>> &matrix, char from, char to) {
+    auto paths = find_all_move_sequences(matrix, from, to);
+    paths = find_shortest_paths(paths);
+    paths = find_simplest_paths(paths);
+    vector<string> out;
+    for (const auto &path: paths) {
+        out.emplace_back(path.begin(), path.end());
+    }
+    return out;
+}
+
+void
+combine(const vector<vector<string>> &input, const string &current_string, vector<string> &results, int index = 0) {
+    if (index == input.size()) {
+        results.push_back(current_string);
+    } else {
+        auto sequences = input[index];
+        for (auto seq: sequences) {
+            combine(input, current_string + seq, results, index + 1);
         }
     }
 }
 
-bool check_if_panic_when_vert_move_first(const Position& from, const Position& to, const Position& panic_pos) {
-    auto [r, c] = from;
-    const auto [dr, dc] = from.distance(to);
-    const auto [pr, pc] = panic_pos;
+vector<string> translate_code_to_keypad_moves(string code) {
+    vector<vector<string>> all_sequences(code.length());
 
-    for (int i = 0; i < abs(dr); i++) {
-        r = (dr < 0) ? r - 1 : r + 1;
-        if (r == pr && c == pc) {
-            return true;
+    auto current = 'A';
+    for (int i = 0; i < code.length(); i++) {
+        auto sequences = get_path_sequences(NUMERIC_CONTROLLER, current, code[i]);
+        for (const auto &seq: sequences) {
+            all_sequences[i].push_back(seq + "A");
         }
+        current = code[i];
     }
-    for (int i = 0; i < abs(dc); i++) {
-        c = (dc < 0) ? c - 1 : c + 1;
-        if (r == pr && c == pc) {
-            return true;
-        }
-    }
-    return false;
+
+    vector<string> combinations;
+    combine(all_sequences, "", combinations);
+    return combinations;
 }
 
-string translate_code_to_keypad_moves(const string &code) {
+vector<string> translate_keypad_sequence_to_another_keypad_moves(const string &sequence) {
+    vector<vector<string>> all_sequences(sequence.length());
+
+    auto current = 'A';
+    for (int i = 0; i < sequence.length(); i++) {
+        auto sequences = get_path_sequences(KEYPAD_CONTROLLER, current, sequence[i]);
+        for (const auto &seq: sequences) {
+            all_sequences[i].push_back(seq + "A");
+        }
+        current = sequence[i];
+    }
+
+    vector<string> combinations;
+    combine(all_sequences, "", combinations);
+    return combinations;
+}
+
+int code_to_number(const string &code) {
     stringstream ss;
-    auto current_pos = numeric_map.find('A')->second;
-    static const Position panic_pos {0, 0};
-
-    for (auto c: code) {
-        const auto next_pos = numeric_map.find(c)->second;
-        const auto& [dr, dc] = current_pos.distance(next_pos);
-
-        bool vert_first = !check_if_panic_when_vert_move_first(current_pos, next_pos, panic_pos);
-
-        if (vert_first) {
-            for (int i = 0; i < abs(dr); i++) {
-                if (dr < 0) ss << '^';
-                else ss << 'v';
-            }
-
-            for (int i = 0; i < abs(dc); i++) {
-                if (dc < 0) ss << '<';
-                else ss << '>';
-            }
-        } else {
-            for (int i = 0; i < abs(dc); i++) {
-                if (dc < 0) ss << '<';
-                else ss << '>';
-            }
-
-            for (int i = 0; i < abs(dr); i++) {
-                if (dr < 0) ss << '^';
-                else ss << 'v';
-            }
-        }
-
-        ss<<'A';
-        current_pos = next_pos;
-    }
-    return ss.str();
-}
-
-string translate_keypad_sequence_to_another_keypad_moves(const string &sequence) {
-    stringstream ss;
-    auto current_pos = kaypad_map.find('A')->second;
-    static const Position panic_pos {3, 0};
-
-    for (auto c: sequence) {
-        const auto next_pos = kaypad_map.find(c)->second;
-        const auto& [dr, dc] = current_pos.distance(next_pos);
-
-        bool vert_first = !check_if_panic_when_vert_move_first(current_pos, next_pos, panic_pos);
-
-        if (vert_first) {
-            for (int i = 0; i < abs(dr); i++) {
-                if (dr < 0) ss << '^';
-                else ss << 'v';
-            }
-
-            for (int i = 0; i < abs(dc); i++) {
-                if (dc < 0) ss << '<';
-                else ss << '>';
-            }
-        } else {
-            for (int i = 0; i < abs(dc); i++) {
-                if (dc < 0) ss << '<';
-                else ss << '>';
-            }
-
-            for (int i = 0; i < abs(dr); i++) {
-                if (dr < 0) ss << '^';
-                else ss << 'v';
-            }
-        }
-
-        ss<<'A';
-        current_pos = next_pos;
-    }
-    return ss.str();
-}
-
-int code_to_number(const string& code) {
-    stringstream ss;
-    for (auto& c : code) {
-        if (isdigit(c)) ss<<c;
+    for (auto &c: code) {
+        if (isdigit(c)) ss << c;
     }
     return stoi(ss.str());
 }
@@ -177,20 +258,23 @@ vector<string> INPUT_DATA = {
 };
 
 int main() {
-    init_numeric_controller_map();
-    init_keypad_controller_map();
-
     long long sum = 0;
-    for (const auto& code: INPUT_DATA) {
-        auto numeric_seq = translate_code_to_keypad_moves(code);
-        auto keypad_1_seq = translate_keypad_sequence_to_another_keypad_moves(numeric_seq);
-        auto keypad_2_seq = translate_keypad_sequence_to_another_keypad_moves(keypad_1_seq);
+    for (const auto &code: INPUT_DATA) {
+        int min_len = INT_MAX;
+        auto numeric_sequences = translate_code_to_keypad_moves(code);
+        for (const auto &numeric: numeric_sequences) {
+            auto keypad_sequences_1 = translate_keypad_sequence_to_another_keypad_moves(numeric);
+            for (const auto &keypad_1: keypad_sequences_1) {
+                auto keypad_sequences_2 = translate_keypad_sequence_to_another_keypad_moves(keypad_1);
+                for (const auto ks_2: keypad_sequences_2) {
+                    min_len = min(min_len, (int) ks_2.length());
+                }
 
-        cout << "(" << keypad_2_seq.size() << " * " <<code_to_number(code)<<")" << endl;
-        sum += keypad_2_seq.size() * code_to_number(code);
+            }
+        }
+        sum += min_len * code_to_number(code);
     }
 
-    cout <<sum;
-
+    cout << sum;
     return 0;
 }
