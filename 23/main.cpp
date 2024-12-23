61e185eaf2ae4297269ac31ef08df1b2f274c3a0
Piotr Głębocki <piotr.glebocki@paramount.com>
2024-12-23 14:59:44 +0100

    23 - solution 2

--- 23/main.cpp
+++ 23/main.cpp
@@ -9,12 +9,12 @@
 
 using namespace std;
 
-std::vector<std::string> split(const std::string &sentence, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(sentence);
+vector<string> split(const string &sentence, char delimiter) {
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(sentence);
 
-    while (std::getline(tokenStream, token, delimiter)) {
+    while (getline(tokenStream, token, delimiter)) {
         tokens.push_back(token);
     }
     return tokens;
@@ -70,36 +70,6 @@ unordered_map<string, unordered_set<string>> create_neighbours_map(
     return nmap;
 }
 
-//unordered_set<string> find_all_connected_vertices(
-//    const string &start,
-//    const unordered_map<string, unordered_set<string>> &nmap
-//) {
-//    unordered_set<string> visited;
-//    visited.insert(start);
-//
-//    queue<string> q;
-//    q.push(start);
-//
-//    while (!q.empty()) {
-//        auto current = q.front();
-//        q.pop();
-//
-//        auto it = nmap.find(current);
-//        if (it == nmap.end()) {
-//            continue;
-//        }
-//        const auto& neighbours = it->second;
-//        for (const auto& neighbour : neighbours) {
-//            if (visited.find(neighbour) == visited.end()) {
-//                visited.insert(neighbour);
-//                q.push(neighbour);
-//            }
-//        }
-//    }
-//
-//    return visited;
-//}
-
 vector<string> get_t_started_vertices(const unordered_set<string> &vertices) {
     vector<string> t_vertices;
     for (const auto &it: vertices) {
@@ -128,8 +98,20 @@ string create_sorted(
     vector tmp = {v1, v2, v3};
     sort(tmp.begin(), tmp.end());
     stringstream ss;
-    for (const auto& it : tmp) {
-        ss << it <<",";
+    for (const auto &it: tmp) {
+        ss << it << ",";
+    }
+    return ss.str();
+}
+
+string create_sorted(
+    unordered_set<string> vertices
+) {
+    vector tmp(vertices.begin(), vertices.end());
+    sort(tmp.begin(), tmp.end());
+    stringstream ss;
+    for (const auto &it: tmp) {
+        ss << it << ",";
     }
     return ss.str();
 }
@@ -161,6 +143,67 @@ vector<string> get_all_three_inner_connect_graphs_from(
     return output;
 }
 
+vector<vector<string>> generate_power_set(const vector<string> &input) {
+    vector<vector<string>> power_set;
+    int n = input.size();
+    int subset_count = (1 << n);
+
+    for (int mask = 0; mask < subset_count; ++mask) {
+        vector<string> subset;
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) {
+                subset.push_back(input[i]);
+            }
+        }
+        power_set.push_back(subset);
+    }
+
+    reverse(power_set.begin(), power_set.end());
+    return power_set;
+}
+
+bool is_complete_graph(
+    const vector<string> &vertices,
+    const unordered_map<string, unordered_set<string>> &nmap
+) {
+    const auto size = vertices.size();
+    for (int i = 0; i < size - 1; i++) {
+        const auto v1 = vertices[i];
+        for (int j = i + 1; j < size; j++) {
+            const auto v2 = vertices[j];
+            if (!are_connected(v1, v2, nmap)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+string get_biggest_complete_graph_from(
+    const string &vertex,
+    const unordered_map<string, unordered_set<string>> &nmap
+) {
+    auto it = nmap.find(vertex);
+    if (it == nmap.end()) {
+        return {};
+    }
+
+    const auto &neighbours = it->second;
+    vector<string> neighbours_list(neighbours.begin(), neighbours.end());
+
+    auto all_possible_sets = generate_power_set(neighbours_list);
+    for (const auto &set: all_possible_sets) {
+        if (is_complete_graph(set, nmap)) {
+            unordered_set<string> res;
+            res.insert(vertex);
+            res.insert(set.begin(), set.end());
+            return create_sorted(res);
+        }
+    }
+
+    return "";
+}
+
 void part_1() {
     auto all_connections = read_connections("23/data");
     auto all_vertices = get_all_vertices(all_connections);
@@ -170,16 +213,35 @@ void part_1() {
     unordered_set<string> results;
     for (const auto &vertex: t_vertices) {
 //        cout << "vertex : "<< vertex << endl;
-        auto graphs = get_all_three_inner_connect_graphs_from(vertex, neighbours_map);
-        for (const auto& graph: graphs) {
-            results.insert(graph);
-        }
+//        auto graphs = get_bigger_inner_connect_graph_from(vertex, neighbours_map);
+//        cout << graphs << endl;
     }
 
     cout << results.size() << endl;
 }
 
+void part_2() {
+    auto all_connections = read_connections("23/data");
+    auto all_vertices = get_all_vertices(all_connections);
+    auto neighbours_map = create_neighbours_map(all_connections);
+
+    vector<string> results;
+    const auto size = all_vertices.size();
+    auto index = 0;
+    for (const auto &vertex: all_vertices) {
+        cout << "processing vertex : " << ++index << "/" << size << endl;
+        auto graph = get_biggest_complete_graph_from(vertex, neighbours_map);
+        results.push_back(graph);
+    }
+
+    sort(results.begin(), results.end(), [](const string &a, const string &b) {
+        return b.length() < a.length();
+    });
+
+    cout << results.front() << endl;
+}
+
 int main() {
-    part_1();
+    part_2();
     return 0;
 }
