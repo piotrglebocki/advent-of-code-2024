4e3acf9ff96779ed0e682bf1e2a62e291394ef9d
Piotr Głębocki <piotr.glebocki@paramount.com>
2024-12-30 14:28:15 +0100

    16 : solution 2 - updated

--- 16/main2.cpp
+++ 16/main2.cpp
@@ -24,6 +24,7 @@ struct CellData {
     int row;
     int col;
     Direction dir;
+    vector<Position> path;
 };
 
 std::tuple<int, int> get_step(Direction dir) {
@@ -95,7 +96,7 @@ bool compare(const CellData &a, const CellData &b) {
     return a.cost > b.cost;
 }
 
-int find(vector<vector<char>> &matrix) {
+vector<vector<Position>> find(vector<vector<char>> &matrix) {
     const auto rows = matrix.size();
     const auto cols = matrix[0].size();
 
@@ -104,11 +105,12 @@ int find(vector<vector<char>> &matrix) {
     auto [start_r, start_c] = start_pos;
 
     priority_queue<CellData, vector<CellData>, decltype(&compare)> pq(compare);
-    pq.push(CellData{0, start_r, start_c, start_dir});
+    pq.push(CellData{0, start_r, start_c, start_dir, {start_pos}});
 
     Cost min_cost = vector(rows, vector(cols, vector(4, INT_MAX)));
-    Previous previous = vector(rows, vector(cols, Position{-1, -1}));
     min_cost[start_r][start_c][(int) start_dir] = 0;
+
+    vector<vector<Position>> shortest_paths;
 
     while (!pq.empty()) {
         auto cell_data = pq.top();
@@ -118,17 +120,22 @@ int find(vector<vector<char>> &matrix) {
         int r = cell_data.row;
         int c = cell_data.col;
         Direction dir = cell_data.dir;
+        auto current_path = cell_data.path;
+
+        if (current_cost > min_cost[r][c][(int) dir]) {
+            continue;
+        }
 
         if (matrix[r][c] == 'E') {
-
-
-            while (previous[r][c] != Position(-1, -1)) {
-                matrix[r][c] = 'O';
-                Position p = previous[r][c];
-                r = get<0>(p);
-                c = get<1>(p);
+            int minimum_cost = min_cost[r][c][(int) dir];
+            if (current_cost < minimum_cost) {
+                min_cost[r][c][(int) dir] = current_cost;
+                shortest_paths.clear();
+                shortest_paths.push_back(current_path);
+            } else if (current_cost == minimum_cost) {
+                shortest_paths.push_back(current_path);
             }
-            return current_cost;
+            continue;
         }
 
         // move
@@ -139,10 +146,11 @@ int find(vector<vector<char>> &matrix) {
 
             if (matrix[new_r][new_c] != '#') {
                 int new_cost = current_cost + 1;
-                if (new_cost < min_cost[new_r][new_c][(int) dir]) {
+                if (new_cost <= min_cost[new_r][new_c][(int) dir]) {
                     min_cost[new_r][new_c][(int) dir] = new_cost;
-                    previous[new_r][new_c] = Position{r, c};
-                    pq.push(CellData{new_cost, new_r, new_c, dir});
+                    auto new_path = current_path;
+                    new_path.emplace_back(new_r, new_c);
+                    pq.push(CellData{new_cost, new_r, new_c, dir, new_path});
                 }
             }
         }
@@ -151,9 +159,9 @@ int find(vector<vector<char>> &matrix) {
         {
             auto new_dir = rotate_cw(dir);
             int new_cost = current_cost + 1000;
-            if (new_cost < min_cost[r][c][(int) new_dir]) {
+            if (new_cost <= min_cost[r][c][(int) new_dir]) {
                 min_cost[r][c][(int) new_dir] = new_cost;
-                pq.push(CellData{new_cost, r, c, new_dir});
+                pq.push(CellData{new_cost, r, c, new_dir, current_path});
             }
 
         }
@@ -162,15 +170,15 @@ int find(vector<vector<char>> &matrix) {
         {
             auto new_dir = rotate_ccw(dir);
             int new_cost = current_cost + 1000;
-            if (new_cost < min_cost[r][c][(int) new_dir]) {
+            if (new_cost <= min_cost[r][c][(int) new_dir]) {
                 min_cost[r][c][(int) new_dir] = new_cost;
-                pq.push(CellData{new_cost, r, c, new_dir});
+                pq.push(CellData{new_cost, r, c, new_dir, current_path});
             }
 
         }
     }
 
-    return INT_MAX;
+    return shortest_paths;
 }
 
 void print(const vector<vector<char>> &matrix) {
@@ -201,10 +209,14 @@ int main() {
 
     unordered_set<string> visited;
 
-    int min_cost = find(matrix);
-    cout << min_cost << endl;
+    auto paths = find(matrix);
 
+    for (const auto &path: paths) {
+        for (const auto &[r, c]: path) {
+            matrix[r][c] = 'O';
+        }
+    }
     print(matrix);
-//    cout << count(matrix);
+    cout << count(matrix);
     return 0;
 }
